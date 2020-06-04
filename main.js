3895cc77f480e8eb1eb63913208a501d5aee510a
Haruka0522 <harukapc1@gmail.com>
2020-06-04 22:30:28 +0900

    ファイル分割

---
+++ main.js
@@ -0,0 +1,11 @@
+var tokyoAlert = function() {
+    alert("東京")
+}
+window.onload = function() {
+    alert("東京")
+    setInterval(tokyoAlert, 3000)
+}
+
+function trigger(){
+	window.alert("東京");
+}
