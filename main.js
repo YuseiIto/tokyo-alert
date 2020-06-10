a3dcb88a21cfa0dd029dafc605de6d3418955c61
Daiki Tomioka <chocolate0785lego@icloud.com>
2020-06-11 02:34:34 +0900

    add change color
    色を変える方法で悩んだ結果、classをつけたり外したりで対応

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-var tokyoAlert = function() {
-	if (state){
-        window.alert("東京")
-	}
+var tokyoAlert = function () {
+        if (state) {
+                window.alert("東京")
+        }
 }
 
-var tokyoRelease = function(){
-	window.alert("解除")
+var tokyoRelease = function () {
+        window.alert("解除")
 }
 
-var trigger = function(){
-	state = true
-	document.bgColor = "red";
-	tokyoAlert()
+var trigger = function () {
+        state = true
+        // document.bgColor = "red";
+        body.classList.add('state1');
+        body.classList.remove('state2');
+        tokyoAlert()
 }
 
-var end = function(){
-	state = false
-	document.bgColor = "white";
-	tokyoRelease()
+var end = function () {
+        state = false
+        // document.bgColor = "white";
+        body.classList.add('state2');
+        body.classList.remove('state1');
+        tokyoRelease()
 }
 
-window.onload = function(){
-	setInterval(tokyoAlert,1500)
+window.onload = function () {
+        setInterval(tokyoAlert, 1500)
 }
