a82ebe9916a7ca985a2dc598ecad6346d80faf77
TomiXRM <chocolate0785lego@icloud.com>
2020-06-12 01:13:35 +0900

    fix at e-chan's contribute

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-var tokyoAlert = function () {
-        if (state) {
-                window.alert("東京")
-        }
-}
+let intervalId = null;
 
-var tokyoRelease = function () {
-        window.alert("解除")
-}
+const tokyoAlert = function () {
+        window.alert("東京");
+};
 
-var trigger = function () {
-        state = true
-        // document.bgColor = "red";
+const releaseTokyo = function () {
+        window.alert("解除");
+};
+
+const trigger = function () {
+        // document.body.style.backgroundColor = "red";
         body.classList.add('state1');
         body.classList.remove('state2');
-        tokyoAlert()
-}
+        tokyoAlert();
+        intervalId = setInterval(tokyoAlert, 1500);
+};
 
-var end = function () {
-        state = false
-        // document.bgColor = "white";
+const end = function () {
+        // document.body.style.backgroundColor = "white";
         body.classList.add('state2');
         body.classList.remove('state1');
-        tokyoRelease()
-}
-
-window.onload = function () {
-        setInterval(tokyoAlert, 1500)
-}
+        clearInterval(intervalId);
+        intervalId = null;
+        releaseTokyo();
+};
